4b83496c98d19de18a7d37655c1445caa63dc423
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2024-01-31 09:03:12 +0200

    btn

--- src/components/counter/UI/Button.jsx
+++ src/components/counter/UI/Button.jsx
@@ -1,16 +1,12 @@
-import { Link } from 'react-router-dom';
-
-export default function Button({ onClick = () => {}, children, outline, type = 'button', to }) {
-  const Element = to ? Link : 'button';
+export default function Button({ onClick = () => {}, children, outline, type = 'button' }) {
   return (
-    <Element
+    <button
       onClick={onClick}
       className={`${
         outline ? 'bg-white text-indigo-500' : 'bg-indigo-500 text-white'
       }  px-6 py-2 text-sm uppercase border font-semibold border-indigo-500 rounded-md`}
-      type={type}
-      to={to}>
+      type={type}>
       {children}
-    </Element>
+    </button>
   );
 }
