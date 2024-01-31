9be2b6200425bbc5ec756629ab2680fe6cdeb10f
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2024-01-31 09:37:16 +0200

    CounterBetter start

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import Counter from './components/counter/Counter';
+import CounterBetter from './components/counter/CounterBetter';
 
 export default function App() {
   return (
     <div className='container mx-auto'>
       <h1 className='text-3xl font-semibold mb-10'>Use Reducer</h1>
       <Counter />
+      <hr className='my-5' />
+      <CounterBetter />
     </div>
   );
 }
---
+++ src/components/counter/CounterBetter.jsx
@@ -0,0 +1,62 @@
+import { useEffect, useState } from 'react';
+
+const btnNormal =
+  'bg-indigo-500 text-white px-6 py-2 text-sm uppercase border font-semibold border-indigo-500 rounded-md';
+const btnOutline =
+  'text-indigo-500 bg-white px-6 py-2 text-sm uppercase border font-semibold border-indigo-500 rounded-md';
+
+const initState = {
+  value: 0,
+};
+
+export default function CounterBetter() {
+  const [state, setState] = useState(initState);
+  console.log('state ===', state);
+  function goUp() {
+    // padidinam state 1
+    // setState(state + 1);
+    // const newState = { value: state.value + 1 }
+    setState((prevState) => {
+      return { value: prevState.value + 1 };
+    });
+  }
+  function goDown() {
+    // pamazinam state 1
+    setState({
+      value: state.value - 1,
+    });
+  }
+
+  function reset() {
+    setState(initState);
+  }
+
+  function upByValue(howMuch) {
+    setState({
+      value: state.value + howMuch,
+    });
+  }
+
+  // padaryti po kortele inputa kuri ivedant keistume <h3 className='text-lg mb-4'>Push ups</h3> reiksme
+
+  return (
+    <div className='inline-block rounded-md bg-indigo-50/50 border-indigo-300 border px-3 py-2 text-center shadow-md'>
+      <h3 className='text-lg mb-4'>Better</h3>
+      <p className='text-5xl mb-5'>{state.value}</p>
+      <div className='flex gap-3'>
+        <button onClick={goUp} className={btnNormal}>
+          Up
+        </button>
+        <button onClick={() => upByValue(5)} className={btnNormal}>
+          Up by 5
+        </button>
+        <button onClick={goDown} className={btnNormal}>
+          Down
+        </button>
+        <button onClick={reset} className={btnOutline}>
+          reset
+        </button>
+      </div>
+    </div>
+  );
+}
