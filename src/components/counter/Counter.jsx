dc23188dce283150c11db5a16a0736b8fab97683
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2024-01-31 08:24:05 +0200

    init

---
+++ src/components/counter/Counter.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react';
+
+const initState = {
+  value: 0,
+};
+
+export default function Counter() {
+  const [state, setState] = useState(initState);
+
+  return <div>Counter</div>;
+}
