99da144655ba8eaae479a6a211344da259fd6ea1
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2024-01-31 08:33:55 +0200

    counter simple

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,10 @@
+import Counter from './components/counter/Counter';
+
 export default function App() {
-  return <div className='container mx-auto'></div>;
+  return (
+    <div className='container mx-auto'>
+      <h1 className='text-3xl font-semibold mb-10'>Use Reducer</h1>
+      <Counter />
+    </div>
+  );
 }
--- src/components/counter/Counter.jsx
+++ src/components/counter/Counter.jsx
@@ -5,7 +5,27 @@ const initState = {
 };
 
 export default function Counter() {
-  const [state, setState] = useState(initState);
+  const [state, setState] = useState(0);
 
-  return <div>Counter</div>;
+  function goUp() {
+    // padidinam state 1
+  }
+  function goDown() {
+    // pamazinam state 1
+  }
+
+  return (
+    <div className='inline-block border px-3 py-2 text-center shadow-md'>
+      <h3 className='text-lg mb-4'>Push ups</h3>
+      <p className='text-5xl mb-5'>{state}</p>
+      <div className='flex gap-3'>
+        <button className='bg-indigo-500 text-white px-6 py-2 text-sm uppercase border font-semibold border-indigo-500 rounded-md'>
+          Up
+        </button>
+        <button className='bg-indigo-500 text-white px-6 py-2 text-sm uppercase border font-semibold border-indigo-500 rounded-md'>
+          Down
+        </button>
+      </div>
+    </div>
+  );
 }
