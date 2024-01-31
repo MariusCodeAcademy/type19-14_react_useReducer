ad855139905119dab9d516c41defa2d6be0d76b8
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2024-01-31 08:37:19 +0200

    more complex state

--- src/components/counter/Counter.jsx
+++ src/components/counter/Counter.jsx
@@ -5,7 +5,7 @@ const initState = {
 };
 
 export default function Counter() {
-  const [state, setState] = useState(0);
+  const [state, setState] = useState(initState);
 
   function goUp() {
     // padidinam state 1
@@ -14,16 +14,21 @@ export default function Counter() {
     // pamazinam state 1
   }
 
+  // reset
+
   return (
     <div className='inline-block border px-3 py-2 text-center shadow-md'>
       <h3 className='text-lg mb-4'>Push ups</h3>
-      <p className='text-5xl mb-5'>{state}</p>
+      <p className='text-5xl mb-5'>{state.value}</p>
       <div className='flex gap-3'>
         <button className='bg-indigo-500 text-white px-6 py-2 text-sm uppercase border font-semibold border-indigo-500 rounded-md'>
           Up
         </button>
         <button className='bg-indigo-500 text-white px-6 py-2 text-sm uppercase border font-semibold border-indigo-500 rounded-md'>
           Down
+        </button>
+        <button className='text-indigo-500 bg-white px-6 py-2 text-sm uppercase border font-semibold border-indigo-500 rounded-md'>
+          reset
         </button>
       </div>
     </div>
