9bab4e6e22810dd82a116fb0551243a9dda015fd
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2024-01-31 09:22:50 +0200

    reset

--- src/components/counter/Counter.jsx
+++ src/components/counter/Counter.jsx
@@ -1,5 +1,10 @@
 import { useEffect, useState } from 'react';
 
+const btnNormal =
+  'bg-indigo-500 text-white px-6 py-2 text-sm uppercase border font-semibold border-indigo-500 rounded-md';
+const btnOutline =
+  'text-indigo-500 bg-white px-6 py-2 text-sm uppercase border font-semibold border-indigo-500 rounded-md';
+
 const initState = {
   value: 0,
 };
@@ -19,14 +24,10 @@ export default function Counter() {
     setState(state - 1);
   }
 
-  useEffect(() => {
-    setTimeout(() => {
-      // setState(state + 1);
-      setState((prevState) => prevState + 1);
-    }, 5000);
-  }, []);
-
-  // reset
+  function reset() {
+    // pamazinam state 1
+    setState(0);
+  }
 
   // padaryti po kortele inputa kuri ivedant keistume <h3 className='text-lg mb-4'>Push ups</h3> reiksme
 
@@ -35,17 +36,13 @@ export default function Counter() {
       <h3 className='text-lg mb-4'>Push ups</h3>
       <p className='text-5xl mb-5'>{state}</p>
       <div className='flex gap-3'>
-        <button
-          onClick={goUp}
-          className='bg-indigo-500 text-white px-6 py-2 text-sm uppercase border font-semibold border-indigo-500 rounded-md'>
+        <button onClick={goUp} className={btnNormal}>
           Up
         </button>
-        <button
-          onClick={goDown}
-          className='bg-indigo-500 text-white px-6 py-2 text-sm uppercase border font-semibold border-indigo-500 rounded-md'>
+        <button onClick={goDown} className={btnNormal}>
           Down
         </button>
-        <button className='text-indigo-500 bg-white px-6 py-2 text-sm uppercase border font-semibold border-indigo-500 rounded-md'>
+        <button onClick={reset} className={btnOutline}>
           reset
         </button>
       </div>
