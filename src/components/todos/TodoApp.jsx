e4370d230f6f09e1f054310649cd9cb385580304
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2024-01-31 11:05:40 +0200

    ADD

--- src/components/todos/TodoApp.jsx
+++ src/components/todos/TodoApp.jsx
@@ -1,18 +1,73 @@
+/* eslint-disable no-case-declarations */
+import { useReducer, useState } from 'react';
+import Button from '../counter/UI/Button';
+
 const initTodos = [
   { id: 1, title: 'Pull ups', isDone: false },
   { id: 2, title: 'Read a book', isDone: true },
   { id: 3, title: 'Buy Bread', isDone: false },
+  { id: 4, title: 'Buy Bread', isDone: false }, // id to dele
 ];
 
-function todoReducer(state, action) {}
+let countId = 5;
+
+function todoReducer(state, action) {
+  console.log('action ===', action);
+  switch (action.type) {
+    case 'DEL':
+      const idToDelete = action.payload;
+      const filtered = state.filter((pObj) => pObj.id !== idToDelete);
+      return filtered;
+    case 'ADD':
+      return [...state, { id: countId++, title: action.payload, isDone: false }];
+    default:
+      console.warn('type nerstas', action);
+      return state;
+  }
+}
 
 export default function TodoApp() {
+  const [state, dispach] = useReducer(todoReducer, initTodos);
+  const [newTodoVal, setNewTodoVal] = useState('');
+  console.log('state ===', state);
+  // console.log('newTodoVal ===', newTodoVal);
+  function handleDelete(idToDelete) {
+    dispach({ type: 'DEL', payload: idToDelete });
+  }
+
+  function handleNewTodo() {
+    console.log('ivesta reiksme', newTodoVal);
+    dispach({ type: 'ADD', payload: newTodoVal });
+  }
+
   return (
     <div>
       <h2 className='text-2xl mb-10'>TodoApp</h2>
 
-      <ul>
-        <li>Do somtething</li>
+      <div className='flex mb-10 gap-1'>
+        <input
+          onChange={(event) => setNewTodoVal(event.target.value)}
+          value={newTodoVal}
+          className='border w-full px-3 py-[6px] rounded-md'
+          type='text'
+          placeholder='new todo'
+        />
+        <Button onClick={handleNewTodo} outline>
+          add
+        </Button>
+      </div>
+
+      <ul className='flex flex-col gap-3'>
+        {state.map((tObj) => (
+          <li key={tObj.id}>
+            <span className='font-semibold text-lg'> {tObj.title}</span>
+            <span>
+              - is
+              {tObj.isDone ? ' Done' : ' NOT Done'}
+            </span>{' '}
+            <Button onClick={() => handleDelete(tObj.id)}>Delete</Button>
+          </li>
+        ))}
       </ul>
     </div>
   );
