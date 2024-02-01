874597c20505905f48fb7b11e2bb5627d9e3a2b0
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2024-02-01 08:58:51 +0200

    Add AddTodo component and refactor TodoApp

--- src/components/todos/OneTodo.jsx
+++ src/components/todos/OneTodo.jsx
@@ -3,7 +3,7 @@ import Button from '../counter/UI/Button';
 export default function OneTodo({ item, onDelete, onDone }) {
   return (
     <li className='grid grid-cols-4 gap-2'>
-      <div>
+      <div className=''>
         <span
           className={
             'font-semibold text-lg ' + `${item.isDone ? 'line-through text-gray-400' : ''}`
@@ -16,6 +16,7 @@ export default function OneTodo({ item, onDelete, onDone }) {
           {item.isDone ? ' Done' : ' NOT Done'}
         </span>{' '}
       </div>
+
       <Button onClick={() => onDelete(item.id)}>Delete</Button>
       <Button onClick={() => onDone(item.id)} outline>
         {item.isDone ? 'Undo' : 'Complete'}
--- src/components/todos/TodoApp.jsx
+++ src/components/todos/TodoApp.jsx
@@ -2,6 +2,7 @@
 import { useReducer, useState } from 'react';
 import Button from '../counter/UI/Button';
 import OneTodo from './OneTodo';
+import AddTodo from './AddTodo';
 
 const initTodos = [
   { id: 1, title: 'Pull ups', isDone: false },
@@ -51,17 +52,16 @@ function todoReducer(state, action) {
 export default function TodoApp() {
   // pasitikrinti ar turiu todos localStorage, jei turiu imu is storage, jei ne initTodos
   const [state, dispach] = useReducer(todoReducer, initTodos);
-  const [newTodoVal, setNewTodoVal] = useState('');
+
   console.log('state ===', state);
 
   const handleDelete = (idToDelete) => {
     dispach({ type: ACT.DEL, payload: idToDelete });
   };
 
-  const handleNewTodo = () => {
+  const handleNewTodo = (newTodoVal) => {
     console.log('ivesta reiksme', newTodoVal);
     dispach({ type: ACT.add, payload: newTodoVal });
-    setNewTodoVal('');
   };
 
   const handleDone = (idToToggle) => {
@@ -75,18 +75,7 @@ export default function TodoApp() {
     <div>
       <h2 className='text-2xl mb-10'>TodoApp</h2>
 
-      <div className='flex mb-10 gap-1'>
-        <input
-          onChange={(event) => setNewTodoVal(event.target.value)}
-          value={newTodoVal}
-          className='border w-full px-3 py-[6px] rounded-md'
-          type='text'
-          placeholder='new todo'
-        />
-        <Button onClick={handleNewTodo} outline>
-          add
-        </Button>
-      </div>
+      <AddTodo onNewTodo={handleNewTodo} />
 
       <ul className='flex flex-col gap-3 '>
         {state.map((tObj) => (
